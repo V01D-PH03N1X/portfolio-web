9f333af384548daba287c4e2f43beef8c1a54c1b
Sebastian Schindler <sebastian@schindlertai.de>
2025-04-26 20:06:32 +0200

    :heavy_plus_sign: Added Vercel Analytics :heavy_plus_sign: Added Vercel Speed Insights
    Signed-off-by: Sebastian Schindler <sebastian@schindlertai.de>

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import {createRoot} from "react-dom/client";
 import "./index.sass";
 import {BrowserRouter, Route, Routes} from "react-router";
 import {Provider} from "@/components/ui/provider";
+import {Analytics} from '@vercel/analytics/next';
+import {SpeedInsights} from '@vercel/speed-insights/next';
 import HomePage from "@/pages/HomePage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics/>
+    <SpeedInsights/>
     <Provider>
       <BrowserRouter>
         <Routes>
